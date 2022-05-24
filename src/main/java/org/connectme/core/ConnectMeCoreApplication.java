8f56f9f26c43b4e74ce0f23eb8b472a1116377fa
Daniel Mehlber <daniel.mehlber@gmail.com>
2022-05-24 23:04:24 +0200

    added authentication filter

--- src/main/java/org/connectme/core/ConnectMeCoreApplication.java
+++ src/main/java/org/connectme/core/ConnectMeCoreApplication.java
@@ -1,9 +1,15 @@
 package org.connectme.core;
 
+import org.connectme.core.authentication.filter.UserAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ConnectMeCoreApplication {
 
 	public static void main(String[] args) {
